8ec09835fd5787fe6aedfd7328f9d272dd4d95b1
Madi Messmore <55211235+madimessmore@users.noreply.github.com>
2021-10-13 16:50:46 -0400

    Update aerodynamics_calcs_template.m

--- aerodynamics_calcs_template.m
+++ aerodynamics_calcs_template.m
@@ -4,15 +4,16 @@ clc, clear, close all;
 format long;
 
 % declaring knowns
-A = ; % frontal projection area found from solidworks, in m^2
-F = ; % force acting on the vehicle, in N
-rho = ; % density of air in kg/m^3
-velocity = ; % in m/s
+A = 0.69051932027271; % frontal projection area found from solidworks
+Fz = 103.323; % z component of force acting on the vehicle, found from ansys
+Fy = ; % y component of force acting on the vehicle, found from ansys
+rho = 1.225; % density of air
+velocity = 20; % m/s
 
 % calculating drag and lift
-Cd = (2*F)/(rho*A*(velocity^2));
+Cd = (2*Fz)/(rho*A*(velocity^2));
 
-Cl = (2*F)/(rho*A*(velocity^2));
+Cl = (2*Fy)/(rho*A*(velocity^2));
 
 printf("\nOur drag coefficient is: %.6f \n", Cd)
 printf("Our lift coefficient is: %.6f \n", Cl)
